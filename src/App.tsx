89fc7830bcab691374b18d1006d520af20bd63c5
e-doschechnikova <evadrobakova@gmail.com>
2022-05-23 17:28:44 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Technologies from "./technologies";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+
+export default App;

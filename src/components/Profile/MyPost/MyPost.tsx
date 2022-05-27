d223596f51b4f84a68563109b27a726356d66c22
e-doschechnikova <evadrobakova@gmail.com>
2022-05-27 18:03:26 +0300

    add props and type

--- src/components/Profile/MyPost/MyPost.tsx
+++ src/components/Profile/MyPost/MyPost.tsx
@@ -10,11 +10,14 @@ export const MyPost = () => {
         <textarea></textarea>
         <button>Add post</button>
         <div className={styles.post}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          <Post message="Hi!" likesCount={5} />
+          <Post message="I`am lost!!!" likesCount={2} />
+          <Post message="Wow! It's really you " likesCount={15} />
+          <Post message="How are you?" likesCount={4} />
+          <Post
+            message="And who is it? And how did I get here?"
+            likesCount={1}
+          />
         </div>
       </div>
     </div>
--- src/components/Profile/MyPost/Post/Post.tsx
+++ src/components/Profile/MyPost/Post/Post.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "../Post/Post.module.css";
 
-export const Post = () => {
+export type postPropsType = {
+  message: string;
+  likesCount: number;
+};
+
+export const Post = (props: postPropsType) => {
   return (
     <div className={styles.item}>
       <img
         src="https://images.unsplash.com/photo-1524568000-769b511e73de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
         alt="avatar"
       />
-      post 1
+      {props.message}
       <div>
-        <span>like</span>
+        <span>{props.likesCount} likes</span>
       </div>
     </div>
   );

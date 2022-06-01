1f1ed15d8c9edec0dab2925dd8f18acc93b4bdbf
e-doschechnikova <evadrobakova@gmail.com>
2022-06-01 18:09:59 +0300

    add browser router

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,19 +5,19 @@ export const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={`${styles.item} ${styles.active}`}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={styles.item}>
-        <a>Messages</a>
+        <a href="/dialogs">Messages</a>
       </div>
       <div className={styles.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={styles.item}>
-        <a>Music</a>
+        <a href="/music">Music</a>
       </div>
       <div className={styles.item}>
-        <a>Setting</a>
+        <a href="/setting">Setting</a>
       </div>
     </nav>
   );

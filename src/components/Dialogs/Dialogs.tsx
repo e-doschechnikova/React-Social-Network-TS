7a16ae560b412c44c0d6b8eba9286d6697eb6e59
e-doschechnikova <evadrobakova@gmail.com>
2022-06-03 19:17:40 +0300

    add Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,5 +2,22 @@ import React from "react";
 import styles from "./Dialogs.module.css";
 
 export const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItem}>
+        <div className={styles.dialog}>Lama Iliya</div>
+        <div className={styles.dialog}>Lama Mama</div>
+        <div className={styles.dialog}>Lama Papa</div>
+        <div className={styles.dialog}>Lama Ira</div>
+        <div className={styles.dialog}>Lama Egor</div>
+      </div>
+      <div className="messages">
+        <div className="message">Hi</div>
+        <div className="message">Where are you?</div>
+        <div className="message">OK!</div>
+        <div className="message">See you later!</div>
+        <div className="message">Bye!</div>
+      </div>
+    </div>
+  );
 };

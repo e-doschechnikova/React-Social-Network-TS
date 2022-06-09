f6473cc0b73e0327d540e5abd78e6b9c9972dbc3
e-doschechnikova <evadrobakova@gmail.com>
2022-06-09 23:43:12 +0300

    add local state

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { type } from "os";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+export type PostPropsType = {
+  id: number;
+  post: string;
+  likesCount: number;
+};
+
+export type PostsPropsType = {
+  posts: Array<PostPropsType>;
+  
+};
+
+let posts: Array<PostPropsType> = [
+  { id: 1, post: "Hi", likesCount: 5 },
+  { id: 2, post: "I`am lost!!!", likesCount: 2 },
+  { id: 3, post: "Wow! It's really you ", likesCount: 15 },
+  { id: 4, post: "How are you?", likesCount: 4 },
+  { id: 5, post: "And who is it? And how did I get here?", likesCount: 1 },
+];
+
+ReactDOM.render(<App posts={posts} />, document.getElementById("root"));

010325ea81239a2a0dfef7d60d5a1dd408870096
e-doschechnikova <evadrobakova@gmail.com>
2022-06-18 16:25:59 +0300

    add new function

--- src/components/Profile/MyPost/MyPost.tsx
+++ src/components/Profile/MyPost/MyPost.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 import { ProfilePagePropsType } from "../../../redux/State";
 import styles from "./MyPost.module.css";
 import { Post } from "./Post/Post";
@@ -8,15 +8,23 @@ export const MyPost = (props: ProfilePagePropsType) => {
     <Post key={post.id} post={post.post} likesCount={post.likesCount} />
   ));
 
+  let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+  const addPost = () => {
+    // let text = newPostElement.current.value;
+    console.log(newPostElement.current?.value);
+    alert("hi");
+  };
+
   return (
     <div className={styles.postsBlock}>
       <h3>My post</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
         <div className={styles.posts}>{postsElements}</div>
       </div>
--- src/redux/State.ts
+++ src/redux/State.ts
@@ -18,7 +18,14 @@ export type DialogPagePropsType = {
   dialogs: Array<DialogPropsType>;
   messages: Array<MessagePropsType>;
 };
-type SidebarPropsType = {};
+export type FriendsSidebarPropsType = {
+  id: number;
+  name: string;
+  avatar: string;
+};
+type SidebarPropsType = {
+  friendsSidebar: Array<FriendsSidebarPropsType>;
+};
 export type RootStatePropsType = {
   profilePage: ProfilePagePropsType;
   dialogsPage: DialogPagePropsType;
@@ -54,7 +61,28 @@ let state: RootStatePropsType = {
       { id: 5, message: "Bye!" },
     ],
   },
-  sidebar: {},
+  sidebar: {
+    friendsSidebar: [
+      {
+        id: 1,
+        name: "Lama Iliya",
+        avatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_eTHoHEyBCjd-p5MA-pUIvItDoaKI7e-A5KsHZrC715mj_B3YtZPF2Cw26mpv2Xzijqk&usqp=CAU",
+      },
+      {
+        id: 3,
+        name: "Lama Papa",
+        avatar:
+          "https://i.pinimg.com/474x/db/0a/15/db0a1537246c4867dd7a312fe23bea12.jpg",
+      },
+      {
+        id: 4,
+        name: "Lama Ira",
+        avatar:
+          "https://i.pinimg.com/736x/fa/7d/6c/fa7d6cf13f3dd106d6d1af501aecb1d8.jpg",
+      },
+    ],
+  },
 };
 
 export default state;

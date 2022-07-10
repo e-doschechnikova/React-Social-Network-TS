55899dabaaea58c4cfe9aea1ce295bf2d38caf8b
e-doschechnikova <evadrobakova@gmail.com>
2022-07-10 22:45:45 +0300

    delete render

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
-import state from "./redux/State";
-import { rerenderEntireTree } from "./render";
+import ReactDOM from "react-dom";
+import App from "./App";
+import state, {
+  addPost,
+  RootStateType,
+  updateNewPostText,
+} from "./redux/State";
+
+export const rerenderEntireTree = (state: RootStateType) => {
+  ReactDOM.render(
+    <App
+      appState={state}
+      addPostCallBack={addPost}
+      updateNewPostText={updateNewPostText}
+    />,
+    document.getElementById("root")
+  );
+};
 
 rerenderEntireTree(state);

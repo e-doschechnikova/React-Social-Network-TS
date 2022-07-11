97ab662ac514419110b189623a9861466185b582
e-doschechnikova <evadrobakova@gmail.com>
2022-07-11 17:13:00 +0300

    fix and refactoring

--- src/redux/State.ts
+++ src/redux/State.ts
@@ -1,8 +1,4 @@
-let rerenderEntireTree = () => {
-  console.log("hello");
-};
-
-///------------------------- type for state----------------------------------\\\
+///------------------------- type for state ----------------------------------\\\
 
 type MessageStateType = {
   id: number;
@@ -37,10 +33,9 @@ export type RootStateType = {
   profilePage: ProfilePageStateType;
   dialogsPage: DialogPageStateType;
   sidebar: SidebarStateType;
-  subscribe: (observer: (state: RootStateType) => void) => void;
 };
 
-///------------------------- type for components-------------------------------\\\
+///------------------------- type for components -------------------------------\\\
 
 export type AppPropsType = {
   appState: RootStateType;
@@ -65,85 +60,103 @@ export type DialogPagePropsType = {
   dialogs: Array<DialogStateType>;
   messages: Array<MessageStateType>;
 };
-///
 
-let state: RootStateType = {
-  profilePage: {
-    messageForNewPost: "hi",
-    posts: [
-      { id: 1, post: "Hi", likesCount: 5 },
-      { id: 2, post: "I`am lost!!!", likesCount: 2 },
-      { id: 3, post: "Wow! It's really you ", likesCount: 15 },
-      { id: 4, post: "How are you?", likesCount: 4 },
-      { id: 5, post: "And who is it? And how did I get here?", likesCount: 1 },
-    ],
+///------------------------- type for store ------------------------------------\\\
+
+export type StoreType = {
+  _state: RootStateType;
+  updateNewPostText: (newText: string) => void;
+  addPost: () => void;
+  _rerenderEntireTree: () => void;
+  subscribe: (observer: () => void) => void;
+  getState: () => RootStateType;
+};
+
+const store: StoreType = {
+  _state: {
+    profilePage: {
+      messageForNewPost: "hi",
+      posts: [
+        { id: 1, post: "Hi", likesCount: 5 },
+        { id: 2, post: "I`am lost!!!", likesCount: 2 },
+        { id: 3, post: "Wow! It's really you ", likesCount: 15 },
+        { id: 4, post: "How are you?", likesCount: 4 },
+        {
+          id: 5,
+          post: "And who is it? And how did I get here?",
+          likesCount: 1,
+        },
+      ],
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Lama Iliya" },
+        { id: 2, name: "Lama Mama" },
+        { id: 3, name: "Lama Papa" },
+        { id: 4, name: "Lama Ira" },
+        { id: 5, name: "Lama Egor" },
+      ],
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "Where are you?" },
+        { id: 3, message: "OK!" },
+        { id: 4, message: "See you later!" },
+        { id: 5, message: "Bye!" },
+      ],
+    },
+    sidebar: {
+      friendsSidebar: [
+        {
+          id: 1,
+          name: "Lama Iliya",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_eTHoHEyBCjd-p5MA-pUIvItDoaKI7e-A5KsHZrC715mj_B3YtZPF2Cw26mpv2Xzijqk&usqp=CAU",
+        },
+        {
+          id: 3,
+          name: "Lama Papa",
+          avatar:
+            "https://i.pinimg.com/474x/db/0a/15/db0a1537246c4867dd7a312fe23bea12.jpg",
+        },
+        {
+          id: 4,
+          name: "Lama Ira",
+          avatar:
+            "https://i.pinimg.com/736x/fa/7d/6c/fa7d6cf13f3dd106d6d1af501aecb1d8.jpg",
+        },
+      ],
+    },
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Lama Iliya" },
-      { id: 2, name: "Lama Mama" },
-      { id: 3, name: "Lama Papa" },
-      { id: 4, name: "Lama Ira" },
-      { id: 5, name: "Lama Egor" },
-    ],
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "Where are you?" },
-      { id: 3, message: "OK!" },
-      { id: 4, message: "See you later!" },
-      { id: 5, message: "Bye!" },
-    ],
+  updateNewPostText(newText: string) {
+    this._state.profilePage.messageForNewPost = newText;
+    this._rerenderEntireTree();
   },
-  sidebar: {
-    friendsSidebar: [
-      {
-        id: 1,
-        name: "Lama Iliya",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_eTHoHEyBCjd-p5MA-pUIvItDoaKI7e-A5KsHZrC715mj_B3YtZPF2Cw26mpv2Xzijqk&usqp=CAU",
-      },
-      {
-        id: 3,
-        name: "Lama Papa",
-        avatar:
-          "https://i.pinimg.com/474x/db/0a/15/db0a1537246c4867dd7a312fe23bea12.jpg",
-      },
-      {
-        id: 4,
-        name: "Lama Ira",
-        avatar:
-          "https://i.pinimg.com/736x/fa/7d/6c/fa7d6cf13f3dd106d6d1af501aecb1d8.jpg",
-      },
-    ],
+  addPost() {
+    const newPost: PostStateType = {
+      id: 5,
+      post: this._state.profilePage.messageForNewPost,
+      likesCount: 0,
+    };
+
+    this._state.profilePage.posts.push(newPost);
+    this._rerenderEntireTree();
+  },
+  _rerenderEntireTree() {
+    console.log("hello");
+  },
+  subscribe(observer) {
+    this._rerenderEntireTree = observer;
+  },
+  getState() {
+    return this._state;
   },
 };
 
-declare global {
-  interface Window {
-    state: RootStateType;
-  }
-}
+// declare global {
+//   interface Window {
+//     state: RootStateType;
+//   }
+// }
+// window.state = store;
 
-window.state = state;
-
-export const addPost = () => {
-  const newPost: PostStateType = {
-    id: 5,
-    post: state.profilePage.messageForNewPost,
-    likesCount: 0,
-  };
-
-  state.profilePage.posts.push(newPost);
-  rerenderEntireTree();
-};
-
-export const updateNewPostText = (newText: string) => {
-  state.profilePage.messageForNewPost = newText;
-  rerenderEntireTree();
-};
-
-export const subscribe = (observer: (state: RootStateType) => void) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;

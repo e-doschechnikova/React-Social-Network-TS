0a0c2651d6e5dde12456b384fc4fd8649568d977
e-doschechnikova <evadrobakova@gmail.com>
2022-08-06 10:52:43 +0300

    fix reducers

--- src/redux/DialogsReducer.ts
+++ src/redux/DialogsReducer.ts
@@ -1,18 +1,21 @@
-import { ActionsTypes, DialogPageStateType } from "./State";
+import {ActionsTypes, DialogPageStateType} from "./State";
 
 const UPDATE_FOR_NEW_MESSAGE = "UPDATE-FOR-NEW-MESSAGE";
 const SEND_MESSAGE = "SEND-MESSAGE";
 
 const DialogsReducer = (state: DialogPageStateType, action: ActionsTypes) => {
-  if (action.type === UPDATE_FOR_NEW_MESSAGE) {
-    state.messageForNewMessage = action.newMessage;
-  } else if (action.type === SEND_MESSAGE) {
-    let newMessage = state.messageForNewMessage;
-    state.messageForNewMessage = "";
-    state.messages.push({ id: 6, message: newMessage });
-  }
-
-  return state;
+    switch (action.type) {
+        case UPDATE_FOR_NEW_MESSAGE:
+            state.messageForNewMessage = action.newMessage;
+            return state
+        case SEND_MESSAGE:
+            let newMessage = state.messageForNewMessage;
+            state.messageForNewMessage = "";
+            state.messages.push({id: 6, message: newMessage});
+            return state
+        default:
+            return state;
+    }
 };
 
 export default DialogsReducer;
--- src/redux/ProfileReducer.ts
+++ src/redux/ProfileReducer.ts
@@ -1,28 +1,30 @@
-import { ActionsTypes, ProfilePageStateType } from "./State";
+import {ActionsTypes, ProfilePageStateType} from "./State";
 
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 type PostStateType = {
-  id: number;
-  post: string;
-  likesCount: number;
+    id: number;
+    post: string;
+    likesCount: number;
 };
 
 const ProfileReducer = (state: ProfilePageStateType, action: ActionsTypes) => {
-  if (action.type === ADD_POST) {
-    const newPost: PostStateType = {
-      id: 5,
-      post: state.messageForNewPost,
-      likesCount: 0,
-    };
-    state.posts.push(newPost);
-    state.messageForNewPost = "";
-  } else if (action.type === UPDATE_NEW_POST_TEXT) {
-    state.messageForNewPost = action.newText;
-  }
-
-  return state;
-};
-
-export default ProfileReducer;
+    switch (action.type) {
+        case ADD_POST:
+            const newPost: PostStateType = {
+                id: 5,
+                post: state.messageForNewPost,
+                likesCount: 0,
+            };
+            state.posts.push(newPost);
+            state.messageForNewPost = "";
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.messageForNewPost = action.newText;
+            return state;
+        default:
+            return state;
+    }
+}
+export default ProfileReducer

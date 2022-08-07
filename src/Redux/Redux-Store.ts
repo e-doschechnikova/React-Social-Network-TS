5c94365b293d890af2b09bffb08fbdefa6b0aad8
e-doschechnikova <evadrobakova@gmail.com>
2022-08-07 20:50:53 +0300

    add redux-store

---
+++ src/Redux/Redux-Store.ts
@@ -0,0 +1,13 @@
+import {combineReducers, createStore} from "redux";
+import ProfileReducer from "./ProfileReducer";
+import DialogsReducer from "./DialogsReducer";
+import SidebarReducer from "./SidebarReducer";
+
+export type RootStateType = ReturnType<typeof rootReducers>
+
+const rootReducers = combineReducers({
+    profilePage: ProfileReducer,
+    dialogsPage: DialogsReducer,
+    sidebar: SidebarReducer
+});
+export const store = createStore(rootReducers)

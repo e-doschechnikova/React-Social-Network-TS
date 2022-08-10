36d9ab91fa334b9c9b16a91c567bd105d0b26d62
e-doschechnikova <evadrobakova@gmail.com>
2022-08-10 15:45:55 +0300

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-import { store } from "./Redux/Store";
+import {store} from "./Redux/Store";
 
 export const rerenderEntireTree = (state: any) => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <App
-        store={store}
-        dispatch={store.dispatch.bind(store)}
-        // appState={store.getState()}
-        // addPostCallBack={store.addPost}
-        // updateNewPostText={store.updateNewPostText}
-      />
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
+    ReactDOM.render(
+        <BrowserRouter>
+            <App
+                store={store}
+                dispatch={store.dispatch.bind(store)}
+                // appState={store.getState()}
+                // addPostCallBack={store.addPost}
+                // updateNewPostText={store.updateNewPostText}
+            />
+        </BrowserRouter>,
+        document.getElementById("root")
+    );
 };
 
 rerenderEntireTree(store.getState());
-store.subscribe(rerenderEntireTree);
+
+store.subscribe(() => {
+    let state = store.getState
+    rerenderEntireTree(state)
+});

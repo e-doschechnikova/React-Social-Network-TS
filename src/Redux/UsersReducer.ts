bda4386ae81acd4351ec949bf2eb4afda342f3a7
e-doschechnikova <evadrobakova@gmail.com>
2022-08-14 21:58:06 +0300

    fix users page and add actions type

--- src/Redux/UsersReducer.ts
+++ src/Redux/UsersReducer.ts
@@ -1,5 +1,10 @@
 import React from 'react';
 import {v1} from "uuid";
+import {text} from "stream/consumers";
+
+
+const FOLLOW = "FOLLOW"
+const UNFOLLOW = "UNFOLLOW"
 
 type LocationType = {
     city: string,
@@ -7,51 +12,63 @@ type LocationType = {
 }
 type UsersType = {
     id: string,
+    followed: boolean
     fullName: string,
     status: string,
     location: LocationType
 }
 
 type UsersPageStateType = {
-    users: UsersType
+    users: Array<UsersType>
 }
 
-const InitialState = {
+const initialState: UsersPageStateType = {
     users: [
         {
             id: v1(),
+            followed: true,
             fullName: "Misha",
             status: "I`m a happy and smiling dog!!!!",
-            location: {city: "Saint Petersbers", country: "Russia"}
+            location: {city: "St. Petersburg", country: "Russia"}
         },
         {
             id: v1(),
+            followed: true,
             fullName: "Lenya",
             status: "I want to eat! :/",
-            location: {city: "Saint Petersbers", country: "Russia"}
+            location: {city: "St. Petersburg", country: "Russia"}
         },
         {
             id: v1(),
+            followed: true,
             fullName: "Tyson",
             status: "I`m a philosopher. Do you want to talk?",
-            location: {city: "Saint Petersbers", country: "Russia"}
+            location: {city: "St. Petersburg", country: "Russia"}
         },
         {
             id: v1(),
+            followed: false,
             fullName: "Murka",
             status: "I`m a good cat! :)))",
-            location: {city: "Saint Petersbers", country: "Russia"}
+            location: {city: "St. Petersburg", country: "Russia"}
         }
     ]
 }
 
-const UsersReducer = () => {
-    let action;
+const UsersReducer = (state: UsersPageStateType = initialState, action: UsersActionsType) => {
     switch (action.type) {
+        case FOLLOW:
+            return {...state, users: state.users.map(user => user.)}
+        case UNFOLLOW:
         default:
             return state
 
     }
 };
+
+
+export type UsersActionsType = ReturnType<typeof FollowAC> | ReturnType<typeof UnfollowAC>
+export const FollowAC = (userId: string) => ({type: FOLLOW, userId})
+export const UnfollowAC = (userId: string) => ({type: UNFOLLOW, userId})
 
 export default UsersReducer;

aa8564219f361482611a7b0ba3b99d95243f8630
e-doschechnikova <evadrobakova@gmail.com>
2022-10-17 17:24:22 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
 const imageSrc = require("../../img/logo.png");
 // declare function require(name: string): string;
 
 export const Header = () => {
-  return (
-    <header className={styles.header}>
-      <img src={imageSrc} alt="" />
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <img src={imageSrc} alt=""/>
+            <div className={styles.loginBlock}>
+                <NavLink to={"/login"}>Login</NavLink>
+            </div>
+        </header>
+    );
 };
